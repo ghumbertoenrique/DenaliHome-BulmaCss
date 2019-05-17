6f5e20461fd5c44fff95462f563e76e80c3f5a3a
ghumbertoenrique <44273075+ghumbertoenrique@users.noreply.github.com>
2019-05-17 12:24:34 -0400

    Add files via upload
    Denali Files

---
+++ js/carousel.js
@@ -0,0 +1,27 @@
+bulmaCarousel.attach('#slider1', {
+  slidesToScroll: 1,
+  slidesToShow:3,
+  infinite:true,
+  navigationKeys:true
+});
+
+bulmaCarousel.attach('#slider2', {
+  slidesToScroll: 1,
+  slidesToShow:1,
+  infinite:true,
+  navigationKeys:true
+});
+
+bulmaCarousel.attach('#slider3', {
+  slidesToScroll: 1,
+  slidesToShow:3,
+  infinite:true,
+  navigationKeys:true
+});
+
+bulmaCarousel.attach('#slider4', {
+  slidesToScroll: 3,
+  slidesToShow:3,
+  infinite:true,
+  navigationKeys:true
+});
